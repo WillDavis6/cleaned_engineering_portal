872f02c040a9ec4841649dd451483f6b84ef9d29
William Davis <william.davis@missionsupport.us>
2024-02-07 15:25:43 -0700

    added class, error handing, and ability to add parts

--- sql.access.py
+++ sql.access.py
@@ -9,17 +9,53 @@ conn = psycopg2.connect(
     port="5432"
 )
 
-cursor = conn.cursor()
+# cursor = conn.cursor()
 
-primary_key_value = '123-abc'
+# primary_key_value = '123-abc'
 
-query = "SELECT * FROM parts WHERE part_id = %s;"
+# query = "SELECT * FROM parts WHERE part_id = %s;"
 
-cursor.execute(query, (primary_key_value,))
+# cursor.execute(query, (primary_key_value,))
 
-row = cursor.fetchone()
+# row = cursor.fetchone()
 
-print(row)
+# print(row)
 
-cursor.close()
-conn.close()
+# cursor.close()
+# conn.close()
+
+class PostGresDB:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def connect(self):
+        try:
+            self.conn
+            print("Connected to data_base")
+        except psycopg2.Error as e:
+            print(f"Error: Unable to connect. {e}")
+
+    def disconnect(self):
+        if self.conn is not None:
+            self.conn.close()
+            print("Successfuly disconnected from database")
+
+    def insert_part(self, table, data):
+        if self.conn is None:
+            print("Error: Not connected to the database")
+            return False
+        try: 
+            cursor = self.conn.cursor()
+            columns = ', '.join(data.keys())
+            placeholders = ', '.join(['%s'] * len(data))
+            query = f"INSERT INTO {table} ({columns}) VALUES ([placeholers]);"
+            cursor.execute(query, list(data.values()))
+            self.conn.commit()
+            cursor.close()
+            print("Part inserted successfully")
+            return True
+        
+        except psycopg2.Error as e:
+            print(f"Error: unable to insert. {e}")
+            return False
+         
